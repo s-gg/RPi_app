d91868ba8ed2eba8929ff30a7e9bc70cbe2d95ca
s-gg <1195551250@qq.com>
2021-01-21 16:08:22 +0800

    2021/1/21

--- app/src/main/java/com/example/rpi_app/MjpgStreamerActivity.java
+++ app/src/main/java/com/example/rpi_app/MjpgStreamerActivity.java
@@ -2,9 +2,9 @@ package com.example.rpi_app;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,20 +13,20 @@ import android.graphics.Color;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
-import android.view.View;
 import android.view.Window;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 
-public class MjpgStreamerActivity extends Activity implements Runnable{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MjpgStreamerActivity extends AppCompatActivity {
     private SurfaceHolder holder;
-    private Thread mythread;
     private Canvas canvas;
     URL videoUrl;
     private String url;
     private int w;
     private int h;
-    private InputStream VideoIS;
+    private InputStream videoIS;
     HttpURLConnection conn;
     Bitmap bmp;
     @Override
@@ -42,20 +42,25 @@ public class MjpgStreamerActivity extends Activity implements Runnable{
         SurfaceView surface = findViewById(R.id.surface);
 
         surface.setKeepScreenOn(true);// 保持屏幕常亮
-        mythread = new Thread(this);
+
+        try {
+            videoUrl=new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         holder = surface.getHolder();
         holder.addCallback(new Callback() {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-                //关闭HttpURLConnection连接
-                conn.disconnect();
             }
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                connection();
-                mythread.start();
+                while(true){
+                    draw();
+                }
             }
 
             @Override
@@ -65,51 +70,60 @@ public class MjpgStreamerActivity extends Activity implements Runnable{
 
             }
         });
+
     }
 
     private void draw() {
-        // TODO Auto-generated method stub
-        try {
-            //得到网络返回的输入流
-            VideoIS = conn.getInputStream();
-            //创建出一个bitmap
-            bmp = BitmapFactory.decodeStream(VideoIS);
-            w=bmp.getWidth();
-            h=bmp.getHeight();
-            canvas = holder.lockCanvas();
-            canvas.drawColor(Color.WHITE);
-            RectF rectf = new RectF(0,0,w,h);
-            canvas.drawBitmap(bmp, null, rectf, null);
-            holder.unlockCanvasAndPost(canvas);
-            //关闭HttpURLConnection连接
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    conn=(HttpURLConnection)videoUrl.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+
+                    videoIS=conn.getInputStream();
+                    bmp=BitmapFactory.decodeStream(videoIS);
+                    UIop(bmp);
+
+                }catch(MalformedURLException e){
+                    e.printStackTrace();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }finally {
+                    if(conn!=null){
+                        conn.disconnect();
+                    }
+                    if(videoIS!=null){
+                        try {
+                            videoIS.close();
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 
-    public void connection(){
-        try {
-            //创建一个URL对象
-            videoUrl=new URL(url);
-            //利用HttpURLConnection对象从网络中获取网页数据
-            conn = (HttpURLConnection)videoUrl.openConnection();
-            //设置输入流
-            conn.setDoInput(true);
-            //连接
-            conn.connect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void UIop(final Bitmap bitmap){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    w=bitmap.getWidth();
+                    h=bitmap.getHeight();
+                    canvas = holder.lockCanvas();
+                    canvas.drawColor(Color.WHITE);
+                    RectF rectf = new RectF(0,0,w,h);
+                    canvas.drawBitmap(bmp, null, rectf, null);
+                    holder.unlockCanvasAndPost(canvas);
 
-    }
-
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        while(true){
-            draw();
-        }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
     }
 
 }
