645a40c7b4628e332067c2fef1798ab7b690755f
s-gg <1195551250@qq.com>
2021-01-22 13:01:36 +0800

    2021/1/21

--- app/src/main/java/com/example/rpi_app/Main2Activity.java
+++ app/src/main/java/com/example/rpi_app/Main2Activity.java
@@ -3,24 +3,46 @@ package com.example.rpi_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main2Activity extends AppCompatActivity {
-
+    Map<String,Object> componentMap=new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initView();
     }
 
-    public void btn_openVideo(View v){
+    protected void initView(){
+        componentMap.put("webview",findViewById(R.id.wv_stream));
+    }
+
+    /**
+     * 获取树莓派快照图片
+     * @param v
+     */
+    public void btn_photo(View v){
         Intent intent=new Intent();
         intent.setAction("com.example.rpi_app.VIDEOPLAY");
-        //intent.setData(Uri.parse("192.168.50.71:8081"));
-        intent.putExtra("CameralURL","http://192.168.50.90:8080/?action=stream");
+        intent.putExtra("CameralURL","http://192.168.50.90:8080/?action=snapshot");
         startActivity(intent);
+    }
+
+    /**
+     * 内置浏览器获取并显示实时视频流
+     * @param v
+     */
+    public void btn_video(View v){
+        WebView wv=(WebView)componentMap.get("webview");
+        wv.setInitialScale(200);
+        wv.loadUrl("http://192.168.50.90:8080/?action=stream");
     }
 
 }
--- app/src/main/java/com/example/rpi_app/MjpgStreamerActivity.java
+++ app/src/main/java/com/example/rpi_app/MjpgStreamerActivity.java
@@ -20,36 +20,36 @@ import android.view.SurfaceView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MjpgStreamerActivity extends AppCompatActivity {
-    private SurfaceHolder holder;
-    private Canvas canvas;
-    URL videoUrl;
-    private String url;
-    private int w;
-    private int h;
-    private InputStream videoIS;
-    private HttpURLConnection conn;
-    private RectF rectF=new RectF();
-    Bitmap bmp;
+    private SurfaceHolder holder;   //用于控制SurfaceView组件
+    private Canvas canvas;          //用于向界面写入图形
+    URL videoUrl;                   //URL地址
+    private String url;             //URL字符串
+    Bitmap bmp;                     //位图，作为写入UI界面的数据类型
+    private int w;                  //位图宽
+    private int h;                  //位图高
+    private InputStream videoIS;    //用于接收树莓派的图片流
+    private HttpURLConnection conn; //用于连接到树莓派
+    private RectF rectF=new RectF();//用于指定往UI上写入图片的尺寸
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mjpg_streamer);
-
+        //从Main2Activity中接收传递过来的url字符串参数
         url = getIntent().getExtras().getString("CameralURL");
-
+        //从布局中获取SurfaceView图片显示对象
         SurfaceView surface = findViewById(R.id.surface);
-
-        surface.setKeepScreenOn(true);// 保持屏幕常亮
-
+        // 保持屏幕常亮
+        surface.setKeepScreenOn(true);
+        //将字符串转为url类型
         try {
             videoUrl=new URL(url);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
+        //控制SurfaceView的生命周期，实现Surface功能函数
         holder = surface.getHolder();
         holder.addCallback(new Callback() {
 
@@ -59,66 +59,76 @@ public class MjpgStreamerActivity extends AppCompatActivity {
 
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                while(true){
                     draw();
-                }
             }
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width,
                                        int height) {
-                // TODO Auto-generated method stub
-
             }
         });
 
     }
 
+    /**
+     * 显示图片
+     */
     private void draw() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    conn=(HttpURLConnection)videoUrl.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.setConnectTimeout(10000);
-                    conn.setReadTimeout(10000);
-              //      conn.setDoInput(true);
-                    videoIS = conn.getInputStream();
-                    bmp = BitmapFactory.decodeStream(videoIS);
-                    UIop(bmp);
-
-                }catch(MalformedURLException e){
-                    e.printStackTrace();
-                }catch(IOException e){
-                    e.printStackTrace();
-                }finally {
-                    if(conn!=null){
-                        conn.disconnect();
-                    }
-                    if(videoIS!=null){
-                        try {
-                            videoIS.close();
-                        } catch(IOException e) {
-                            e.printStackTrace();
+                    try {
+                        //与树莓派建立连接
+                        conn = (HttpURLConnection) videoUrl.openConnection();
+                        conn.setDoInput(true);
+                        conn.connect();
+                        //获取输入流
+                        videoIS = conn.getInputStream();
+                        //将输入流的数据解码成位图
+                        bmp = BitmapFactory.decodeStream(videoIS);
+                        //因为子线程无法修改UI，故需要传递位图参数在主线程修改UI界面
+                        UIop(bmp);
+                    }catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        // 释放所用到的流对象
+                        if(conn!=null){
+                            conn.disconnect();
+                        }
+                        if (videoIS != null) {
+                            try {
+                                videoIS.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
-            }
+
         }).start();
     }
+
+    /**
+     * 进入主线程修改UI界面
+     * @param bitmap
+     */
 
     private void UIop(final Bitmap bitmap){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    //获取图片的尺寸，以便通过进行RectF显示
                     w=bitmap.getWidth();
                     h=bitmap.getHeight();
+                    //获取画笔并加锁，开始在SurfaceView上写入图片
                     canvas = holder.lockCanvas();
                     canvas.drawColor(Color.WHITE);
                     rectF.set(0,0,w,h);
                     canvas.drawBitmap(bmp, null, rectF, null);
+                    //解锁画笔，图片显示完毕
                     holder.unlockCanvasAndPost(canvas);
                 } catch (Exception ex) {
                     ex.printStackTrace();
